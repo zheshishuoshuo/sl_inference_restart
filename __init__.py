3f3b55abc5c3cdd72ff02ec9afa1ead1a8087f99
Shuo <90323232+zheshishuoshuo@users.noreply.github.com>
2025-08-05 00:05:04 +0800

    Add plotting module and integrate into main

--- __init__.py
+++ __init__.py
@@ -6,6 +6,7 @@ Minimal package initialisation exposing the core inference utilities.
 from .run_mcmc import run_mcmc
 from .likelihood import log_prior, log_likelihood, log_posterior
 from .utils import selection_function, mag_likelihood
+from .plotting import plot_chain
 
 __all__ = [
     "run_mcmc",
@@ -14,6 +15,7 @@ __all__ = [
     "log_posterior",
     "selection_function",
     "mag_likelihood",
+    "plot_chain",
 ]
 
 __version__ = "0.1"
